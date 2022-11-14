f2b8baa969e3001f2c3dee26564ba8b8126b77c4
y.ohta <y.ohta@waiohtanoMacBook-Pro.local>
2022-11-14 13:34:28 +0900

    string_add.phpを書いた

---
+++ string_add.php
@@ -0,0 +1,13 @@
+<?php
+
+$string_1 = "Hello ";
+$string_2 = "world.";
+
+
+$string_dot = $string_1.$string_2."<br>";
+echo $string_dot;
+
+
+
+$string_dquote = "{$string_1}{$string_2}<br>";
+echo $string_dquote;
