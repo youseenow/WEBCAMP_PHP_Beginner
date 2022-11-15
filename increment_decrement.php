fcac36a2160ae1649d278969fae69f8b87293292
y.ohta <y.ohta@waiohtanoMacBook-Pro.local>
2022-11-15 18:43:57 +0900

    increment_decrement.phpを書いた

---
+++ increment_decrement.php
@@ -0,0 +1,19 @@
+<?php
+
+$i = 1;
+echo "{$i}<br><br>";
+
+$i = $i + 1;
+echo "{$i}<br><br>";
+
+$i += 1;
+echo "{$i}<br><br>";
+
+++$i;
+echo "{$i}<br><br>";
+
+--$i;
+echo "{$i}<br><br>";
+
+$i -= 1;
+echo "{$i}<br><br>";
