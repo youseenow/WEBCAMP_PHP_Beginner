b74dae3dece166a466df0a6d989b3e2b32bde23b
y.ohta <y.ohta@waiohtanoMacBook-Pro.local>
2022-11-25 10:44:56 +0900

    read_file_all.phpとread_file_each.phpを書いた

---
+++ read_file_all.php
@@ -0,0 +1,7 @@
+<?php
+
+$file_name = __FILE__;
+
+$file_string = file_get_contents($file_name);
+
+var_dump($file_string);
---
+++ read_file_each.php
@@ -0,0 +1,10 @@
+<?php
+
+$file_name = __FILE__;
+
+$file_obj = new SplFileObject($file_name);
+
+foreach($file_obj as $no => $line) {
+    echo "{$no}： $line";
+}
+
