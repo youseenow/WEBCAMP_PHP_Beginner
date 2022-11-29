883bb542933b79235747dcdb43e43f27c715134c
y.ohta <y.ohta@waiohtanoMacBook-Pro.local>
2022-11-29 21:53:35 +0900

    課題提出_3回目

--- PHP_kadai/fibonacci.php
+++ PHP_kadai/fibonacci.php
@@ -2,15 +2,22 @@
 
 
 
+$numbers = [0,1];
+
 $i = 0;
 $j = 1;
-$k = 0;
 
 
-while($i < 10000) {
-    echo "{$k}\n";
+
+while($j < 10000) {
     $k = $i + $j;
+    $numbers[] = $k;
     $i = $j;
     $j = $k;
 }
 
+
+
+foreach($numbers as $number) {
+    echo "{$number}\n";
+}
